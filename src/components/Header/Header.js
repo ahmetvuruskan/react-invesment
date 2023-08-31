eac1ed07c26f1701194910642cfdff1b5ca3ef80
ahmetvuruskan <ahmetvrskn@gmail.com>
2023-08-31 22:12:55 +0300

    feature:exploded components

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "../assets/investment-calculator-logo.png";
+
+const Header = props => {
+    return <header className="header">
+        <img src={props.logo} alt="logo"/>
+        <h1>Investment Calculator</h1>
+    </header>
+}
+
+export default Header;
